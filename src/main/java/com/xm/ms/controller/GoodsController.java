2fbdba3a3610414130da8db3fe8d6d77d561e5f6
momo <1486573@qq.com>
2019-08-06 22:38:42 +0800

    页面缓存与URL缓存

--- src/main/java/com/xm/ms/controller/GoodsController.java
+++ src/main/java/com/xm/ms/controller/GoodsController.java
@@ -1,6 +1,8 @@
 package com.xm.ms.controller;
 
 import com.xm.ms.domain.MiaoshaUser;
+import com.xm.ms.redis.GoodsKey;
+import com.xm.ms.redis.RedisService;
 import com.xm.ms.result.Result;
 import com.xm.ms.service.GoodsService;
 import com.xm.ms.service.MiaoshaUserService;
@@ -10,10 +12,14 @@ import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.thymeleaf.spring4.context.SpringWebContext;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 import java.util.List;
@@ -37,6 +43,15 @@ public class GoodsController {
     @Autowired
     GoodsService goodsService;
 
+    @Autowired
+    RedisService redisService;
+
+    @Autowired
+    ThymeleafViewResolver thymeleafViewResolver;
+
+    @Autowired
+    ApplicationContext applicationContext;
+
    /* @RequestMapping("/to_list")
     String toList(HttpServletResponse response,Model model,
                    @CookieValue(value = MiaoshaUserService.COOKIE_NAME_TOKEN, required = false) String cookieToken,
@@ -50,18 +65,41 @@ public class GoodsController {
         return "goods_list";
     }*/
 
-    @RequestMapping("/to_list")
-    public String toList(Model model, MiaoshaUser user) {
+    @RequestMapping(value = "/to_list", produces = "text/html")
+    @ResponseBody
+    public String toList(HttpServletRequest request, HttpServletResponse response, Model model, MiaoshaUser user) {
         model.addAttribute("user",user);
         //查询商品列表
         List<GoodsVo> goodsList = goodsService.listGoodsVo();
         model.addAttribute("goodsList",goodsList);
-        return "goods_list";
+//        return "goods_list";
+        //取缓存
+        String html = redisService.get(GoodsKey.getGoodsList, "", String.class);
+        if (!StringUtils.isEmpty(html)) {
+            return html;
+        }
+        //手动渲染
+        SpringWebContext ctx = new SpringWebContext(request, response, request.getServletContext(),
+                request.getLocale(), model.asMap(), applicationContext);
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_list", ctx);
+        if (!StringUtils.isEmpty(html)) {
+            redisService.set(GoodsKey.getGoodsList, "", html);//页面缓存（所有用户访问同一页面）,缓存时间为60秒
+        }
+        return html;
     }
 
-    @RequestMapping("/to_detail/{goodsId}")
-    public String toDetail(Model model, MiaoshaUser user, @PathVariable("goodsId") long goodsId) {
+    @RequestMapping(value = "/to_detail/{goodsId}", produces = "text/html")
+    @ResponseBody
+    public String toDetail(HttpServletResponse response, HttpServletRequest request, Model model, MiaoshaUser user, @PathVariable("goodsId") long goodsId) {
         model.addAttribute("user",user);
+
+        //取缓存
+        String html = redisService.get(GoodsKey.getGoodsDetail, ""+goodsId, String.class);
+        if (!StringUtils.isEmpty(html)) {
+            return html;
+        }
+        //
+
         GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
         model.addAttribute("goods",goods);
 
@@ -85,7 +123,14 @@ public class GoodsController {
         model.addAttribute("miaoshaStatus",miaoshaStatus);
         model.addAttribute("remainSeconds",remainSeconds);
 
-        return "goods_detail";
+//        return "goods_detail";
+        SpringWebContext ctx = new SpringWebContext(request, response, request.getServletContext(),
+                request.getLocale(), model.asMap(), applicationContext);
+        html = thymeleafViewResolver.getTemplateEngine().process("goods_detail", ctx);
+        if (!StringUtils.isEmpty(html)) {
+            redisService.set(GoodsKey.getGoodsDetail, ""+goodsId, html);//URL缓存(不同goodsId访问页面不同),缓存时间为60秒
+        }
+        return html;
     }
 
 
---
+++ src/main/java/com/xm/ms/redis/GoodsKey.java
@@ -0,0 +1,20 @@
+package com.xm.ms.redis;
+
+/**
+ * @author xmbian
+ * @version 1.0
+ * Email: 1486573@qq.com
+ * date: 2019-08-06 - 21:40
+ * description:
+ */
+public class GoodsKey extends BasePrefix {
+
+    private GoodsKey(int expireSeconds, String prefix) {
+        super(expireSeconds, prefix);
+    }
+
+    public static GoodsKey getGoodsList = new GoodsKey(60,"gl");
+    public static GoodsKey getGoodsDetail = new GoodsKey(60,"gd");
+
+
+}
